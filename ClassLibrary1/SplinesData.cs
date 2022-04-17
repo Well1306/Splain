9571665f832f0a0c7626ec4bed7613cce8b4b549
well1 <well1@DESKTOP-1L4CER0>
2022-04-17 08:03:39 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/SplinesData.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace ClassLibrary1
+{
+    public class SplinesData
+    {
+        public MeasuredData Md { get; set; }
+        public SplineParameters Sp { get; set; }
+        public double? Integral { get; set; } = null;
+        public double? Der_l { get; set; } = null;
+        public double? Der_r { get; set; } = null;
+        public SplinesData(MeasuredData md, SplineParameters sp)
+        {
+            Md = md; Sp = sp;
+        }
+        public SplinesData()
+        {
+            Md = new(); Sp = new();
+        }
+
+        [DllImport("\\..\\..\\..\\..\\x64\\Debug\\Splain.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double Interpolate(int length, double[] points, double[] func, double[] res, double[] der, int gridlen, double[] grid, double[] limits, double[] integrals);
+        public double[] Splain(ref double a, ref double[] Int) 
+        {
+            double[] splain = new double[3 * Sp.N];
+            a = Interpolate(Md.N, Md.Grid, Md.Measured, splain, new double[] { Sp.Deriative_l, Sp.Deriative_r }, Sp.N, new double[] { Md.Start, Md.End }, new double[] { Md.Int_Start, Md.Int_End }, Int);
+            Integral = Int[0];
+            Der_l = splain[1];
+            Der_r = splain[^2];
+            return splain;
+
+        }
+        public string IntInfo
+        {
+            get
+            {
+                if (Integral == null || Der_l == null || Der_r == null)
+                    return "Интеграл не посчитан";
+                return $"Интеграл посчитан на отрезке [{Md.Int_Start}; {Md.Int_End}]\nЗначение: {Integral}\nПроизводные на границах:\n\tСлева {Der_l}\n\tСправа {Der_r}";
+
+            }
+        }
+
+        public ObservableCollection<string>? Str
+        {
+            get
+            {
+                if (!Md.Iszeros)
+                {
+                    Md._str = new();
+                    for (int i = 0; i < Md.N; i++) Md._str.Add($"x[{i + 1}]: {Md.Grid[i]:f8}\t\ty[{i + 1}]: {Md.Measured[i]:f8}");
+                    return Md._str;
+                }
+                return null;
+            }
+            set
+            {
+                Md._str = value;
+            }
+        }
+
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassLibrary1;
+using System.Globalization;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    class DoubleStrConv : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                double val = (double)value;
+                return $"{val:0.0}";
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show($"Unexpected error: {error.Message}.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return "DoubleStrConv: ERROR";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                string val = value as string;
+                return double.Parse(val);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show($"Unexpected error: {error.Message}.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return "DoubleStrConv: ERROR";
+            }
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+
+        MeasuredData md = new();
+        SplineParameters sp = new();
+        public ViewData vd { get; set; } = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = this;
+        }
+        private void TextBox(object sender, TextCompositionEventArgs e)
+        {
+            vd.Changed = true;
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            vd.Clear();
+        }
+        private void MeasuredData_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !vd.Data.Md.SetErr() && !vd.Data.Sp.SetErr();
+        }
+
+        private void MeasuredData_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            vd.Data.Md.Func = vd.SpfList.selectedFunc.func;
+            vd.Changed = true;
+            vd.MdSetGrid();
+            vd.Chart.AddPlot(vd.Data.Md.Grid, vd.Data.Md.Measured, 2, "Points");
+        }
+
+        private void Splines_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !vd.Data.Md.SetErr() && !vd.Data.Sp.SetErr();
+        }
+
+        private void Splines_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            double a = 213123;
+            double[] Int = new double[1];
+            double[] r = vd.Splain(ref a, ref Int);
+
+            double[] res = new double[vd.Data.Sp.N];
+            for (int i = 0; i < res.Length; i++)
+                res[i] = r[0 + 3 * i];
+            double[] grid = new double[vd.Data.Sp.N];
+            for (int i = 0; i < vd.Data.Sp.N; i++)
+                grid[i] = vd.Data.Md.Start + i * (vd.Data.Md.End - vd.Data.Md.Start) / (vd.Data.Sp.N - 1);
+
+            vd.Chart.AddPlot(grid, res, 1, "Splain1");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(vd.Data.Str[2]);
+        }
+
+        private void TextBox2_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            int val;
+            if (!Int32.TryParse(e.Text, out val) && e.Text != ",")
+            {
+                e.Handled = true; // отклоняем ввод
+            }
+        }
+        private void TextBox1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            int val;
+            if (!Int32.TryParse(e.Text, out val))
+            {
+                e.Handled = true; // отклоняем ввод
+            }
+        }
+    }
+    public static class Cmd
+    {
+        public static readonly RoutedUICommand MeasuredData = new
+            (
+                "MeasuredData",
+                "MeasuredData",
+                typeof(Cmd),
+                new InputGestureCollection()
+                {
+                    new KeyGesture(Key.D1, ModifierKeys.Control)
+                }
+            );
+
+        public static readonly RoutedUICommand Splines = new
+        (
+            "Splines",
+            "Splines",
+            typeof(Cmd),
+            new InputGestureCollection()
+            {
+                    new KeyGesture(Key.D2, ModifierKeys.Control)
+            }
+        );
+    }
+}
